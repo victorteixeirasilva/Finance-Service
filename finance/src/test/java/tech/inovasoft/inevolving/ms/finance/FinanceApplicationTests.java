708d0b64d6a4d11c65cdeee3eac129a4483fe674
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-20 13:59:10 -0300

    feat: Iniciando projeto Spring

---
+++ finance/src/test/java/tech/inovasoft/inevolving/ms/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.inovasoft.inevolving.ms.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
