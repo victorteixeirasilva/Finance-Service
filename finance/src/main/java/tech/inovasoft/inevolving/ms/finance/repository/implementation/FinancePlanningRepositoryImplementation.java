43f65db094b141b658dfcd56733133f548ca1bec
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-23 13:50:18 -0300

    feat: Desenvolvendo o minimo para o teste passar, (savePlanningForUser).

--- finance/src/main/java/tech/inovasoft/inevolving/ms/finance/repository/implementation/FinancePlanningRepositoryImplementation.java
+++ finance/src/main/java/tech/inovasoft/inevolving/ms/finance/repository/implementation/FinancePlanningRepositoryImplementation.java
@@ -1,16 +1,22 @@
 package tech.inovasoft.inevolving.ms.finance.repository.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import tech.inovasoft.inevolving.ms.finance.domain.model.FinancePlanning;
 import tech.inovasoft.inevolving.ms.finance.repository.interfaces.FinancePlanningRepository;
+import tech.inovasoft.inevolving.ms.finance.repository.interfaces.jpa.FinancePlanningRepositoryJPA;
 
 import java.util.UUID;
 
 public class FinancePlanningRepositoryImplementation implements FinancePlanningRepository {
+
+    @Autowired
+    private FinancePlanningRepositoryJPA repository;
+
     @Override
     public FinancePlanning savePlanningForUser(UUID idUser) {
-        //TODO: Desenvolver método para o teste passar
+        var newPlanning = new FinancePlanning(idUser, 0.0);
+        return repository.save(newPlanning);
         //TODO: Refatorar Código
-        return null;
     }
 
     @Override
