66ab85b9322eb67ade4083e70f352957c811c593
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-11-20 10:12:59 -0300

    NWF - Desenvolver sistema de autorização de microserviços, sem precisar da Rede do Docker para deixar escalavel

---
+++ finance/src/main/java/tech/inovasoft/inevolving/ms/finance/service/client/Auth_For_MService/dto/AuthAuthenticationRequest.java
@@ -0,0 +1,4 @@
+package tech.inovasoft.inevolving.ms.finance.service.client.Auth_For_MService.dto;
+
+public record AuthAuthenticationRequest(String microServiceName, String superSecret) {
+}
---
+++ finance/src/main/java/tech/inovasoft/inevolving/ms/finance/service/client/Auth_For_MService/dto/AuthLoginResponse.java
@@ -0,0 +1,4 @@
+package tech.inovasoft.inevolving.ms.finance.service.client.Auth_For_MService.dto;
+
+public record AuthLoginResponse(String BearerToken) {
+}
